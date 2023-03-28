b194781ba6ff1e30329e8121346ab4f187c423e4
brotto_bhmg <cbx01@hotmail.it>
2023-03-28 22:41:55 +0200

    create php library
    move file in folder

--- php/PoolSpace.php
+++ php/PoolSpace.php
@@ -1,1 +1,71 @@
+<?php
+class PoolSpace
+{
+  // Properties
+  public $wallet;
+  private $serialized_data;
+  private $endpoint;
 
+  function __construct($wallet)
+  {
+    $this->wallet = $wallet;
+    $this->endpoint = 'https://api-mainnet.pool.space/api/farms/' . $wallet . '/';
+    $this->serialized_data=json_decode(file_get_contents($this->endpoint));
+  }
+
+  // Methods
+  function requestData()
+  {
+    return $this->serialized_data;
+  }
+
+  function unpaidBalance()
+  {
+    return $this->serialized_data->unpaidBalanceInXCH;
+  }
+
+  function totalPaidInXCH()
+  {
+    return $this->serialized_data->totalPaidInXCH;
+  }
+
+  function blocksFound()
+  {
+    return $this->serialized_data->blocksFound;
+  }
+
+  function estimatedPlotSizeTiB()
+  {
+    return $this->serialized_data->estimatedPlotSizeTiB;
+  }
+
+  function estimatedPlots()
+  {
+    return $this->serialized_data->estimatedPlots;
+  }
+
+  function pendingPoints()
+  {
+    return $this->serialized_data->pendingPoints;
+  }
+
+  function rank()
+  {
+    return $this->serialized_data->rank;
+  }
+
+  function globalPendingPoints(){
+    return $this->serialized_data->globalPendingPoints;
+  }
+
+  function printAll(){
+      foreach ($this->serialized_data as $key => $value) {
+        if (is_array($value))
+          print_r($value);
+        else
+          echo "$key ==> $value \n";
+      }
+    
+    
+  }
+}
---
+++ php/example.php
@@ -0,0 +1,8 @@
+<?php
+
+include "PoolSpace.php";
+
+$apple = new PoolSpace('0x40f32d481b8bec1e5c48a3fb1254c378812d94bbeee5930f55d1571fc9d36772');
+$apple->printAll();
+
+?>
